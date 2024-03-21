99e01da8190107021a9b17dacce91849c604e4c6
Ps-99 <163538996+Ps-99@users.noreply.github.com>
2024-03-22 01:33:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -185,3 +185,8 @@ app.get("/re.js", async (req, res) => {
     res.send(response);
   }
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen({port: PORT}, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
